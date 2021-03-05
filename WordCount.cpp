60b14fdc0cd2907ca77de27c0c2e60225e78ee06
zhengkaixin <44114392+Zhengkaixin@users.noreply.github.com>
2021-03-05 22:39:06 +0800

    Add files via upload

--- WordCount.cpp
+++ WordCount.cpp
@@ -13,24 +13,28 @@ using namespace std;
 int main(int argc, char* argv[])
 {
 	fstream stream;
-	stream.open("input.txt", ios::in);
+	stream.open("argv[1]", ios::in);
+	if (!stream) {
+		cout << "fail to open" << endl;
+		return -1;
+	}
 	charCount charcount;
 	wordCount wordcount;
 	rowCount rowcount;
 	charcount.charcount(stream);
 	stream.close();
-	stream.open("input.txt", ios::in);
+	stream.open("argv[1]", ios::in);
 	wordcount.wordcount(stream);
 	stream.close();
-	stream.open("input.txt", ios::in);
+	stream.open("argv[1]", ios::in);
 	rowcount.rowcount(stream);
 	stream.close();
-	stream.open("input.txt", ios::in);
+	stream.open("argv[1]", ios::in);
 	repeatCount repeatcount(wordcount.getNumber());
 	repeatcount.wordFrequence(stream);
 	stream.close();
 
-	stream.open("output.txt", ios::out);
+	stream.open("argv[2]", ios::out);
 	stream << charcount.getNumber() << endl;
 	stream << wordcount.getNumber() << endl;
 	stream << rowcount.getNumber() << endl;
